39341f64d8bb873cf948a9c890bb6f2449a3b9f1
Inwoo <lys91892@gmail.com>
2025-05-18 04:58:44 +0900

    [Feat/Intro] 회원가입

--- core/data/src/main/kotlin/com/hiearth/fullquiz/core/data/UserRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/hiearth/fullquiz/core/data/UserRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.hiearth.fullquiz.core.local.datasource.PreferenceDataSource
 import com.hiearth.fullquiz.core.model.Interests
 import com.hiearth.fullquiz.core.model.NickNameCheck
 import com.hiearth.fullquiz.core.network.datasource.UserDataSource
+import com.hiearth.fullquiz.core.network.model.ApiResponse
 import javax.inject.Inject
 
 internal class UserRepositoryImpl @Inject constructor(
@@ -19,9 +20,15 @@ internal class UserRepositoryImpl @Inject constructor(
     }
 
     override suspend fun setUser(nickName: String, interest: Interests) {
-        userDataSource.signUp(nickName)
-        preferenceDataSource.setNickname(nickName)
-        preferenceDataSource.setInterest(interest)
+        when(val result = userDataSource.signUp(nickName)) {
+            is ApiResponse.Failure.HttpError -> throw Throwable(result.message)
+            is ApiResponse.Failure.NetworkError -> throw Throwable(result.throwable)
+            is ApiResponse.Failure.UnknownApiError -> throw Throwable(result.throwable)
+            is ApiResponse.Success -> {
+                preferenceDataSource.setNickname(nickName)
+                preferenceDataSource.setInterest(interest)
+            }
+        }
     }
 
     override fun getNickname(): String {
